67100e076d25d914f87179c823f938962770f290
lightningl0l <matiasalejandroazar@gmail.com>
2025-06-25 21:24:34 +1000

    Update media player.py

--- media player.py
+++ media player.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from os import listdir, path
 import pygame.mixer as sound #pygame needs to be installed
 from mutagen.mp3 import MP3 #mutagen needs to be installed
-from time import sleep
+import time
 import threading
 
 #region setup
@@ -33,20 +33,29 @@ playMenu = tk.Canvas(
 playMenu.pack(side='bottom')
 
 def song_timer(song):
-    l = MP3(song).info.length
-    playMenu.delete('musiclength')
-    playMenu.create_text(
-        4, 4, text=':'.join(((str(int(l - (l % 3600)) // 3600).zfill(2)),
-        (str(int(l - (l - l % 3600) - (l % 60)) // 60).zfill(2)),
-        (str(int(l % 60)).zfill(2)))),
-        font=('Consolas', 8),
-        anchor='w', fill='#ffffff', tags='musiclength')
-    playMenu.create_text(
-        int(playMenu.cget('width')) - 4, 4, text=':'.join(((str(int(l - (l % 3600)) // 3600).zfill(2)),
-        (str(int(l - (l - l % 3600) - (l % 60)) // 60).zfill(2)),
-        (str(int(l % 60)).zfill(2)))),
-        font=('Consolas', 8),
-        anchor='e', fill='#ffffff', tags='musiclength')
+    bpfs = 0 #time the song has been playing for in seconds
+    l = MP3(song).info.length #song length in seconds
+
+    for n in range(int(l)):
+        bpf = (int(bpfs // 3600), int(bpfs % 3600 // 60), int(bpfs % 60))
+        timeL = ':'.join((str(bpf[0]).zfill(2), str(bpf[1]).zfill(2),str(bpf[2]).zfill(2)))
+        rem = int(l - bpfs)
+        rem_h = rem // 3600
+        rem_m = (rem % 3600) // 60
+        rem_s = rem % 60
+        timeR = ':'.join((str(rem_h).zfill(2), str(rem_m).zfill(2), str(rem_s).zfill(2)))
+        playMenu.delete('musiclength')
+        playMenu.create_text(
+            4, 4, text=timeL,
+            font=('Consolas', 8),
+            anchor='w', fill='#ffffff', tags='musiclength')
+        playMenu.create_text(
+            int(playMenu.cget('width')) - 4, 4,
+            text=timeR,
+            font=('Consolas', 8),
+            anchor='e', fill='#ffffff', tags='musiclength')
+        bpfs += 1
+        time.sleep(1)
 #endregion
 
 #region side menu
@@ -113,11 +122,14 @@ def click_on_song(i):
     global songCurrentlyPlaying
     mainMenu.delete('chosenbox')
     if 18 < i.y - scroll < (len(playlistContent[0]) * 36 + 18):
+        sound.music.stop()
         sound.music.load(playlistContent[0][(i.y - scroll - 18) // 36])
         sound.music.play()
         songCurrentlyPlaying = (i.y - scroll - 18) // 36
         song_text()
-        song_timer(playlistContent[0][(i.y - scroll - 18) // 36])
+        timings = threading.Thread(target=song_timer, daemon=True, args=(playlistContent[0][(i.y - scroll - 18) // 36], ))
+        timings.start()
+        # song_timer(playlistContent[0][(i.y - scroll - 18) // 36])
 
 def menu_scroll(i):
     global scroll
